f02016abb799b43a8d504550c6c1fab74fc44521
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-24 21:14:51 +0500

    Initial Commit

---
+++ SlideInModalView/SlideInModalViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideInModalViewApp.swift
+//  SlideInModalView
+//
+//  Created by Arkasha Zuev on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlideInModalViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
